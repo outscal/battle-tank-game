819401d9c069bc54d9717d05fa4a95b47eac2d35
Shikhar <shikhar.kr007@gmail.com>
2020-04-04 15:40:40 +0530

    monoSingletonGeneric, tank service, mvc created

---
+++ Assets/Scripts/MonoSingletonGeneric.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingletonGeneric : MonoBehaviour
+{
+    public static MonoSingletonGeneric instance;
+
+    private void Awake()
+    {
+        CreateSingleton();
+    }
+
+
+    private void CreateSingleton()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TankService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankService : MonoSingletonGeneric
+{
+    public TankView tankView;
+
+    private void Start()
+    {
+        TankModel model = new TankModel(5, 100f);
+        TankController tank = new TankController(model, tankView);
+    }
+
+}
