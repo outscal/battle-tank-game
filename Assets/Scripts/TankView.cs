940e27e9bf099b5781cd63e8123f201b32391224
Shikhar <shikhar.kr007@gmail.com>
2020-04-05 01:25:02 +0530

    tank controll wasd, tank collision with gravity, tires dust added

--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 public class TankView : MonoBehaviour
 {
+
+     float horizontalInput;
+     float verticalInput;
+    public float rotatingSpeed;
+    public float movingSpeed;
+    private Vector3 currentEulerAngles;
+    private Vector3 currentTankSpeed;
+    public Rigidbody rb;
+
     private void Start()
     {
         Debug.Log("TankView Has Started");   
     }
+
+    private void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxisRaw("HorizontalUI");
+        verticalInput = Input.GetAxisRaw("VerticalUI");
+        moveTank();
+    }
+
+
+    private void moveTank()
+    {
+        //transform.rotation = new Vector3(horizontalInput* rotatingSpeed, )
+        currentEulerAngles += new Vector3(0, horizontalInput,0) * Time.deltaTime * rotatingSpeed;
+        transform.eulerAngles = currentEulerAngles;
+        //currentTankSpeed += new Vector3(0, 0, verticalInput) * Time.deltaTime * movingSpeed;
+        //transform.forward = currentTankSpeed;
+        rb.velocity = transform.forward * verticalInput * Time.deltaTime * movingSpeed;
+
+    }
+
 }
