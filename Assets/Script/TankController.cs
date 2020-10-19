b5d090d284d19f661376a10fd2c56a5d70a6fdfb
Suvamjyoti_Panda <likun54@gmail.com>
2020-10-19 10:33:56 +0530

    monoSingeltonGeneric|TankCotroller|cameraController

---
+++ Assets/Script/TankController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoSingeltonGeneric<TankController>
+{
+    private Rigidbody _rigidBody;
+
+    [SerializeField]
+    private float _moveForce;
+
+    private void Awake() {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+
+        if(Input.GetKey(KeyCode.W)){
+            _rigidBody.AddForce(_moveForce*Vector3.forward);
+        }
+
+        if(Input.GetKey(KeyCode.S)){
+            _rigidBody.AddForce(_moveForce*-Vector3.forward);
+        }
+        
+        if(Input.GetKey(KeyCode.A)){
+            _rigidBody.AddForce(_moveForce*-Vector3.right);
+        }
+        
+        if(Input.GetKey(KeyCode.D)){
+            _rigidBody.AddForce(_moveForce*Vector3.right);
+        }
+        
+
+    }
+
+}
---
+++ Assets/Script/cameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    void Update()
+    {
+        transform.position= target.position + offset;
+    }
+}
