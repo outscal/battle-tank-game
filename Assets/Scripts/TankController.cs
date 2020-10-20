ea96cbc18f5348a92ea94f358e97abd668e8f3a4
Manas Gangwani <manasjg1996@gmail.com>
2020-10-20 14:47:29 +0530

    Moved project to 2018.4.23f1 and created a generic singleton

---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,16 @@
+﻿using Singleton;
+using UnityEngine;
+
+public class TankController : MonoSingletonGeneric<TankController>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        Debug.Log("Creating tank");
+    }
+
+    public void CallTest()
+    {
+        Debug.Log(this.gameObject.name);
+    }
+}
