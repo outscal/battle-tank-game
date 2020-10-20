89bc0edbca649f1b7dbe5e6e8fdbe1e4ad31229f
Manas Gangwani <manasjg1996@gmail.com>
2020-10-20 18:07:15 +0530

    Added basic tank movement

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -1,16 +1,39 @@
 ﻿using Singleton;
 using UnityEngine;
 
-public class TankController : MonoSingletonGeneric<TankController>
+namespace Player
 {
-    protected override void Awake()
+    [RequireComponent(typeof(Rigidbody))]
+    public class TankController : MonoSingletonGeneric<TankController>
     {
-        base.Awake();
-        Debug.Log("Creating tank");
-    }
+        [SerializeField]
+        FloatingJoystick joystick;
 
-    public void CallTest()
-    {
-        Debug.Log(this.gameObject.name);
+        [SerializeField]
+        float moveSpeed;
+
+        [SerializeField]
+        Transform chassis;
+
+        Rigidbody rb;
+        float horizontalInput, verticalInput;
+        protected override void Awake()
+        {
+            base.Awake();
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (horizontalInput != 0 || verticalInput != 0)
+                rb.AddForce(chassis.transform.forward * moveSpeed * Time.deltaTime,ForceMode.Impulse);
+        }
+        private void Update()
+        {
+             horizontalInput = joystick.Horizontal;
+             verticalInput = joystick.Vertical;
+            if(horizontalInput!=0 || verticalInput != 0)
+            chassis.transform.localEulerAngles = new Vector3(0, (Mathf.Atan2(horizontalInput, verticalInput) * Mathf.Rad2Deg)+45f, 0);
+        }
     }
 }
