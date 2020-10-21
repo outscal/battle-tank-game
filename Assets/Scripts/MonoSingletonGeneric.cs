164c8d7fd5565913f2aebf506a430ea001c275ae
Prashant Dombale <prashant.dombale@gmail.com>
2020-10-21 14:25:50 +0530

    Added the MonoSingleton script with Generic

---
+++ Assets/Scripts/MonoSingletonGeneric.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingletonGeneric<T> : MonoBehaviour where T : MonoSingletonGeneric<T>
+{
+    private static T instance;
+    public static T Instance { get { return instance; } }
+
+    protected virtual void Awake()
+    {
+        if (instance == null) { instance = (T)this; }
+        else {
+            Debug.LogError("Duplicate singleton creation detected, deleting duplicate");
+            Destroy(this);
+        }
+    }
+}
