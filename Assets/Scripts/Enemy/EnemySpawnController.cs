a1630fc82615a1e3218c271006635f6770178711
Prashant Dombale <prashant.dombale@gmail.com>
2020-10-29 15:34:25 +0530

    Changed the enemy spawn position to random throughout the map and improved the camera

--- Assets/Scripts/Enemy/EnemySpawnController.cs
+++ Assets/Scripts/Enemy/EnemySpawnController.cs
@@ -8,27 +8,31 @@ public class EnemySpawnController : MonoSingletonGeneric<EnemySpawnController>
     [SerializeField]
     private Button spawnButton;
     private int count;
+    private bool noTank;
     // Start is called before the first frame update
     void Start()
     {
         count = 0;
         Invoke("SpawnEnemy", 2f);
+        noTank = false;
         spawnButton.onClick.AddListener(SpawnEnemy);
     }
     private void SpawnEnemy() {
-        if (transform.childCount == 0)
+        if (transform.childCount==0)
         {
             if (count < 4)
             {
-                GameObject enemy = Instantiate(basicEnemy, transform);
+                GameObject enemy = Instantiate(basicEnemy, new Vector3(UnityEngine.Random.Range(-48,48), 0, UnityEngine.Random.Range(-48,47)), transform.rotation);
                 Renderer[] rend = enemy.GetComponentsInChildren<Renderer>();
+                enemy.transform.SetParent(transform);
                 for (int i = 0; i < rend.Length; i++) rend[i].material.color = Color.red;
                 enemy.tag="Enemy";
                 count++;
             }
             else {
-                GameObject enemy = Instantiate(basicEnemy, transform);
+                GameObject enemy = Instantiate(basicEnemy, new Vector3(UnityEngine.Random.Range(-48, 48), 0, UnityEngine.Random.Range(-48, 47)), transform.rotation);
                 Renderer[] rend = enemy.GetComponentsInChildren<Renderer>();
+                enemy.transform.SetParent(transform);
                 for (int i = 0; i < rend.Length; i++) rend[i].material.color = Color.black;
                 enemy.transform.localScale *= 3f;
                 enemy.tag ="BossEnemy";
