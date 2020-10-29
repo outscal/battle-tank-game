ffa7fc29dfc35389577cb9d8c605d23dd3f3b41c
Prashant Dombale <prashant.dombale@gmail.com>
2020-10-29 16:34:52 +0530

    Added an arrow to the player tank which points in the direction of the spawned enemy

---
+++ Assets/Scripts/TargetFinder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFinder : MonoBehaviour
+{
+    Animator anim;
+    private GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GameObject.Find("EnemySpawner");
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        EnemyController point = enemy.GetComponentInChildren<EnemyController>();
+        if (point != null)
+        {
+            anim.SetBool("enemyFound", true);
+            var dir = point.transform.position - transform.position;
+            var angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+        }
+        else
+        {
+            anim.SetBool("enemyFound", false);
+        }
+    }
+}
