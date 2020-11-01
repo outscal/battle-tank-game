b1d02721d89a31044c9d888f92e894b8109b6953
Suvamjyoti_Panda <likun54@gmail.com>
2020-11-01 11:16:57 +0530

    Feature3-JoystickControl

---
+++ Assets/Script/Tank/TankController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoSingeltonGeneric<TankController>
+{
+    [SerializeField]
+    private Joystick joystick;
+
+    internal float vertical{get;private set;}
+    internal float horizontal{get;private set;}
+    
+//`````````````````````````````````````````````````````````````````````````````````````````````````````````
+//`````````````````````````````````````````````````````````````````````````````````````````````````````````
+    private void FixedUpdate() {
+
+        vertical = joystick.Vertical;
+        horizontal = joystick.Horizontal;
+    }
+
+}
---
+++ Assets/Script/Tank/TankModel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class TankModel: MonoSingeltonGeneric<TankController>
+{
+    public float Speed = 5f;
+
+    internal float getSpeed(){
+        return Speed;
+    }
+}
