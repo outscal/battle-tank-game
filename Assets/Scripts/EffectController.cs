b9aa8760fae2de407513d5350beab9ffddbaa304
Manas Gangwani <manasjg1996@gmail.com>
2020-11-08 14:31:57 +0530

    Cleaned up effect controller

--- Assets/Scripts/EffectController.cs
+++ Assets/Scripts/EffectController.cs
@@ -5,7 +5,6 @@ namespace Effects
     [RequireComponent(typeof(ParticleSystem))]
     public class EffectController : MonoBehaviour
     {
-        ParticleSystem particleEffect;
         public void playEffect(Vector3 pos)
         {
             transform.position = pos;
