817ecb9b3b3317ea40fa041f2dc4a3e4874f565d
Suvamjyoti_Panda <likun54@gmail.com>
2020-11-13 18:58:49 +0530

    ShootingMechanics

---
+++ Assets/Scripts/Tank/TankShoot.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankShoot : MonoBehaviour
+{
+    public Rigidbody m_Shell;                   // Prefab of the shell.
+    public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
+    public float m_MinLaunchForce = 15f;        // The force given to the shell if the fire button is not held.
+    public float m_MaxLaunchForce = 30f;        // The force given to the shell if the fire button is held for the max charge time.
+    public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
+
+
+    private float m_CurrentLaunchForce;         // The force that will be given to the shell when the fire button is released.
+    private float m_ChargeSpeed;                // How fast the launch force increases, based on the max charge time.
+    private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
+
+
+    private void OnEnable()
+    {
+        // When the tank is turned on, reset the launch force and the UI
+        m_CurrentLaunchForce = m_MinLaunchForce;
+    }
+
+
+    private void Start ()
+    {
+
+        // The rate that the launch force charges up is the range of possible forces by the max charge time.
+        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+    }
+
+
+    private void Update ()
+    {
+
+        // If the max force has been exceeded and the shell hasn't yet been launched...
+        if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+        {
+            // ... use the max force and launch the shell.
+            m_CurrentLaunchForce = m_MaxLaunchForce;
+            Fire ();
+        }
+        // Otherwise, if the fire button has just started being pressed...
+        else if (Input.GetButtonDown ("Fire"))
+        {
+            // ... reset the fired flag and reset the launch force.
+            m_Fired = false;
+            m_CurrentLaunchForce = m_MinLaunchForce;
+
+        }
+        // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
+        else if (Input.GetButton ("Fire") && !m_Fired)
+        {
+            // Increment the launch force and update the slider.
+            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+
+        }
+        // Otherwise, if the fire button is released and the shell hasn't been launched yet...
+        else if (Input.GetButtonUp ("Fire") && !m_Fired)
+        {
+            // ... launch the shell.
+            Fire ();
+        }
+    }
+
+
+    private void Fire ()
+    {
+        // Set the fired flag so only Fire is only called once.
+        m_Fired = true;
+
+        // Create an instance of the shell and store a reference to it's rigidbody.
+        Rigidbody shellInstance =
+            Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+        // Set the shell's velocity to the launch force in the fire position's forward direction.
+        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward; ;
+
+        // Reset the launch force.  This is a precaution in case of missing button events.
+        m_CurrentLaunchForce = m_MinLaunchForce;
+    }
+}
