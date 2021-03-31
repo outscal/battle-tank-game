b2769f2a905ad7c6d6ed877c5f05e7acbd688745
Vaishali Sharma <33980820+vaishali161197@users.noreply.github.com>
2021-03-31 15:06:52 +0530

    joystick and singleton

---
+++ Assets/scripts/MonoSingletonGeneric.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingletonGeneric<T> : MonoBehaviour where T : MonoSingletonGeneric<T>
+{
+    private static T instance;
+    public static T Instance { get { return instance; } }
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = (T)this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
+
