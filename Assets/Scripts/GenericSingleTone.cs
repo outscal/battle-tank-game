b179571a411f4f902d15e4e7cc98dbdc041c3be7
nil17593 <navgekar18@gmail.com>
2021-08-03 16:38:59 +0530

    generic singletone

---
+++ Assets/Scripts/GenericSingleTone.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Outscal.BattleTank
+{
+    /// <summary>
+    ///Generic singletone class 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GenericSingleTone<T> : MonoBehaviour where T : GenericSingleTone<T>
+    {
+        private static T instance;
+
+        public static T Instance { get { return instance; } }
+
+        protected virtual void Awake()
+        {
+            if (instance == null)
+            {
+                instance = (T)this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    /// <summary>
+    /// player class taht inherited from base singletone class 
+    /// </summary>
+    public class PlayerTank : GenericSingleTone<PlayerTank>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            //game logic
+        }
+    }
+    /// <summary>
+    /// enemy class that is inherited from base generic singletone
+    /// </summary>
+    public class EnemyTank : GenericSingleTone<EnemyTank>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            //game logic
+        }
+    }
+}
