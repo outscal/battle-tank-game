cc3b3d6ac9125f333ed1a049d6b0ebe85b43f963
nil17593 <navgekar18@gmail.com>
2021-08-06 16:34:22 +0530

    implemented MVC for tank spawning.

---
+++ Assets/Scripts/MVC/TankService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Outscal.BattleTank
+{
+    /// <summary>
+    /// service class that 
+    /// </summary>
+    public class TankService : MonoBehaviour
+    {
+        [SerializeField] private TankView tankView;
+        void Start()
+        {
+            TankModel model = new TankModel(5, 100f);
+            TankController tank = new TankController(model, tankView);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/MVC/TankView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Outscal.BattleTank
+{
+    /// <summary>
+    /// this class handles the tankview logic 
+    /// it inherits Monobehaviours
+    /// </summary>
+    public class TankView : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("tank view created");
+        }
+    }
+}
