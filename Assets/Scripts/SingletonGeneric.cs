38cdb22e5cff901ea61c03058fa9b0cf910ae008
SACHIN <sachinwagde67@gmail.com>
2021-12-02 19:24:38 +0530

    Singleton Generic for Player and Enemy tank

---
+++ Assets/Scripts/SingletonGeneric.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonGeneric<T> : MonoBehaviour where T : SingletonGeneric<T>
+{
+    private static SingletonGeneric<T> instance;
+    public static SingletonGeneric<T> Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else 
+        {
+            instance = (T)this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
