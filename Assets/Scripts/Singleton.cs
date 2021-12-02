6fba09e7a6cbf99c2f6dcb92401261328d4c839c
Rahul Yadav <devmethyl@outlook.com>
2021-12-02 18:59:46 +0530

    Added Singleton class

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T: Singleton<T>
+{
+    private static T _instance;
+    public static  T _Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = (T)this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
