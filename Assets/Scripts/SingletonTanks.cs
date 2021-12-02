548788b9b2f50a94eacb7408d34f0329178c3509
SACHIN <sachinwagde67@gmail.com>
2021-12-02 18:27:37 +0530

    singleton

---
+++ Assets/Scripts/SingletonTanks.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonTanks<T> : MonoBehaviour where T : SingletonTanks<T>
+{
+    private static SingletonTanks<T> instance;
+    public static SingletonTanks<T> Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else 
+        {
+            instance = (T)this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
