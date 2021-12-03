dc5bf91ad4167cdc68ec0210ef8b7f39da16ab01
Dhiraj57 <58966263+Dhiraj57@users.noreply.github.com>
2021-12-03 10:58:04 +0530

    Basic tank movement

---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoSingletonGeneric<TankController>
+{
+    [SerializeField]
+    private Joystick joystick;
+
+    [SerializeField]
+    private Rigidbody rigidbody;
+
+    private float dirX, dirY, moveSpeed;
+
+    private void Start()
+    {
+        moveSpeed = 0.2f;
+    }
+
+    private void Update()
+    {
+        dirX = joystick.Horizontal * moveSpeed;
+        dirY = joystick.Vertical * moveSpeed;
+
+        rigidbody.transform.localPosition += Vector3.forward * dirY;
+        rigidbody.transform.localPosition += Vector3.right * dirX;
+
+         // rigidbody.velocity += new Vector3(dirX, 0, dirY);
+    }
+
+}
