b7257bafd1ad02dc5dae75936b3790c47cdd410a
Rahul Yadav <devmethyl@outlook.com>
2021-12-12 21:00:26 +0530

    Added minor changes

---
+++ Assets/Scripts/MVC/Tank/NewEditorScript1.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.MVC.Tank
+{
+    public class NewEditorScript1 : ScriptableObject
+    {
+        [MenuItem("Tools/MyTool/Do It in C#")]
+        static void DoIt()
+        {
+            EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 0.10f;
     private float horizontalMove = 0.0f;
     public Joystick joystick;
+    private Transform position;
 
     // Start is called before the first frame update
     void Start()
