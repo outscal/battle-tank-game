42870b1d0e2daa85027435cdc5f60943b6567203
daisy13 <divyam2752000@gmail.com>
2021-12-12 18:47:52 +0530

    commit new

--- Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
+++ Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
@@ -3,12 +3,13 @@ using UnityEngine.PostProcessing;
 
 namespace UnityEditor.PostProcessing
 {
-    [CustomPropertyDrawer(typeof(MinAttribute))]
+
+    [CustomPropertyDrawer(typeof(UnityEngine.MinAttribute))]
     sealed class MinDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            MinAttribute attribute = (MinAttribute)base.attribute;
+            UnityEngine.MinAttribute attribute = (UnityEngine.MinAttribute)base.attribute;
 
             if (property.propertyType == SerializedPropertyType.Integer)
             {
@@ -26,4 +27,5 @@ namespace UnityEditor.PostProcessing
             }
         }
     }
+
 }
