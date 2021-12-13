b26b30d806abf1d16b2c03e186f68217bac40466
_Justrishi <rishimud@gmail.com>
2021-12-13 20:58:52 +0530

    Implemented the basic Joystick control for android build

---
+++ Assets/Scripts/TankControllerScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControllerScript : MonoSingletonGeneric<TankControllerScript>
+{
+    [SerializeField]
+    private Joystick joystick;
+
+    [SerializeField]
+    private Rigidbody rigidbody;
+
+    private float dirX, dirY, moveSpeed;
+
+    private void Start()
+    {
+        moveSpeed = 0.2f;
+    }
+
+    private void Update()
+    {
+        dirX = joystick.Horizontal * moveSpeed;
+        dirY = joystick.Vertical * moveSpeed;
+
+        rigidbody.transform.localPosition += Vector3.forward * dirY;
+        rigidbody.transform.localPosition += Vector3.right * dirX;
+
+        // rigidbody.velocity += new Vector3(dirX, 0, dirY);
+    }
+
+}
