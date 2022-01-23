0614069098e09473243aec77e0608f316d92b8e0
nannapuraju Karthikeya <karthik.suraj2619@gmail.com>
2022-01-23 19:05:38 +0530

    geberic singleton created

---
+++ Assets/Scripts/playerTank/playerTank.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace palyertank
+{
+
+
+    public class playerTank : TankSingeton<playerTank>
+    {
+        public int motion;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void move()
+        {
+
+        }
+    }
+}
