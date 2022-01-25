ade26a7b66c999380e91a1f757ae97145143c389
Vishal Mishra <66241481+vishmi4092@users.noreply.github.com>
2022-01-25 16:20:18 +0530

    Player and Enemy Tank Singleton and Player Movement using Stick
    Player and Enemy Tank Singleton and Player Movement using Stick

---
+++ Assets/Scripts/PlayerTank.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTank : TankSingletonGeneric<PlayerTank>
+{
+
+    public float speed;
+    public Joystick joystick;
+
+    private void Update()
+    {
+        transform.position += new Vector3(joystick.Horizontal*speed*Time.deltaTime, 0, joystick.Vertical*speed*Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/TankSingletonGeneric.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSingletonGeneric<T> : MonoBehaviour
+{
+    public static TankSingletonGeneric<T> instance;
+
+    public void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
