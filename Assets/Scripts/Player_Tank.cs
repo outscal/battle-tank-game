577081b7428b22058809bfb278b5563b75412468
gansari231 <ansarigufraan0@gmail.com>
2022-02-14 18:41:23 +0530

    Commit - 1
    Used singleton and generics

---
+++ Assets/Scripts/Player_Tank.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Tank : Singleton_Generic<Player_Tank>
+{
+    protected override void Awake()
+    {
+        Debug.Log("In player tank!!!");
+        base.Awake();
+    }
+}
---
+++ Assets/Scripts/Singleton_Generic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton_Generic<T> : MonoBehaviour where T: Singleton_Generic<T>
+{
+    private static T instance;
+    public static T Instance { get { return instance; } }
+
+    protected virtual void Awake()
+    {
+        if(instance == null)
+        {
+            instance = (T)this;
+        }
+        else
+        {
+            Debug.LogError("Duplicate singleton creating!!!");
+            Destroy(this);
+        }
+    }
+}
