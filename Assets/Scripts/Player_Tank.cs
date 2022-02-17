8c39d7f745f03f89de8182b9397156c57af16b2a
gansari231 <ansarigufraan0@gmail.com>
2022-02-17 20:20:43 +0530

    Updated Tank movement
    Added rotation and smooth movement

--- Assets/Scripts/Player_Tank.cs
+++ Assets/Scripts/Player_Tank.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class Player_Tank : Singleton_Generic<Player_Tank>
 {
+    public Joystick Tank_joystick;
+    float horizontalInput, verticalInput, speed = 7.0f, rotationSpeed = 150.0f;
     protected override void Awake()
     {
-        Debug.Log("In player tank!!!");
         base.Awake();
+    }
+
+    private void Update()
+    {
+        Player_Movement();
+    }
+
+    void Player_Movement()
+    {
+        horizontalInput = Tank_joystick.Horizontal * 5;
+        verticalInput = Tank_joystick.Vertical * 5;
+
+        if (horizontalInput > 0 || horizontalInput < 0 || verticalInput > 0 || verticalInput < 0)
+        {
+            Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+            moveDirection.Normalize();
+
+            transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+
+            if (moveDirection != Vector3.zero)
+            {
+                Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            }
+        }
     }
 }
