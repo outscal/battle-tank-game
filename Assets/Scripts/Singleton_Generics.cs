624e381d016843851a7885eaebbe479e43cff6fc
Zepar99 <pratikkborse@gmail.com>
2022-02-17 11:25:00 +0530

    singleton2

--- Assets/Scripts/Singleton_Generics.cs
+++ Assets/Scripts/Singleton_Generics.cs
@@ -8,7 +8,6 @@ public class Singleton_Generic<T> : MonoBehaviour where T : Singleton_Generic<T>
 
     public static T Instance { get { return instance; } }
 
-
     protected virtual void Awake()
     {
         if (instance != null)
