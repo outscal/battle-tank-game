285c76f61782e9e9a26155acd29fb1b2dc87c47a
Zepar99 <pratikkborse@gmail.com>
2022-02-19 18:41:06 +0530

    MVC1

---
+++ Assets/Scripts/MVC/TankService.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankService : Singleton_Generic<TankService>
+{
+    public TankView tankView;
+
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        CreateNewTank();
+    }
+
+    private TankController CreateNewTank()
+    {
+        TankModel tankModel = new TankModel(5, 100f);
+        TankController tank = new TankController(tankModel, tankView);
+        return tank;
+    }
+}
