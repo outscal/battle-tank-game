0155d601818ed311f46eeba8d9da49e8135b2359
gansari231 <ansarigufraan0@gmail.com>
2022-02-20 22:49:37 +0530

    Update added MVC

---
+++ Assets/Scripts/Tank/TankController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController
+{
+    private TankModel tankModel { get; }
+    private TankView tankView { get; }
+
+    public TankController(TankModel tankModel, TankView tankView)
+    {
+        this.tankModel = tankModel;
+        this.tankView = GameObject.Instantiate<TankView>(tankView);
+    }
+}
