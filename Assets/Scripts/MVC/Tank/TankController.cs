d3c21a5d59304a097cf95eb41ae69c655bceaed6
daisy13 <divyam2752000@gmail.com>
2022-04-14 02:50:47 +0530

    state

--- Assets/Scripts/MVC/Tank/TankController.cs
+++ Assets/Scripts/MVC/Tank/TankController.cs
@@ -19,25 +19,25 @@ public class TankController
         TankView = GameObject.Instantiate<TankView>(tankPrefab);
         rbTank = TankView.GetComponent<Rigidbody>();
     }
-
+    //SetCamera Reference
     public void SetCamera(Camera cameraSet)
     {
         camera = cameraSet;
         camera.transform.SetParent(TankView.Turret.transform);
     }
-
+    //Set joystick Reference
     public void SetJoystick(Joystick rtJoystick, Joystick ltJoystick)
     {
         rightJoystick = rtJoystick;
         leftJoystick = ltJoystick;
     }
-
+    //PlayerTank health
     public void SetHealthUI()
     {
         TankView.healthSlider.value = TankModel.Health;
         TankView.healthFillImage.color = Color.Lerp(TankView.minHealthColour, TankView.maxHealthColour, TankModel.Health / byDefaultHealth);
     }
-
+    //PlayerTank take damage
     public void TakeDamage(int damage)
     {
         byDefaultHealth = TankModel.Health;
@@ -48,7 +48,7 @@ public class TankController
             OnDeath();
         }
     }
-
+    //PlayerTankDeath 
     public void OnDeath()
     {
         TankView.isTankLive = true;
@@ -60,24 +60,24 @@ public class TankController
         TankView.DestroyTank();
         DestroyGameObjects();
     }
-
+    //call when player death todestroy all remaning objects
     public void DestroyGameObjects()
     {
         DestroyEnemyObjects();
         DestroyGroundObjects();
 
     }
-
+    //coroutines for playerdeath and destroy other objects
     private async void DestroyEnemyObjects()
     {
-        //await new WaitForSeconds(2f);
+       // await new WaitForSeconds(2f);
         GameObject enemyTank = GameObject.FindGameObjectWithTag("EnemyTank");
         enemyTank.GetComponent<EnemyTankView>().enemyTankController.OnEnemyDeath();
     }
 
     private async void DestroyGroundObjects()
     {
-        //await new WaitForSeconds(3f);
+        
         GameObject ground = GameObject.FindGameObjectWithTag("Ground");
         DestroyGround(ground);
         //await new WaitForSeconds(0.05f);
@@ -88,13 +88,14 @@ public class TankController
     {
         TankView.DestroyGround(gameObject);
     }
-
-    public void UpdateShootBullet()
+    // create bullet instance to shotbullet
+    public void ShootBullet()
     {
         BulletService.Instance.CreateBullet(TankView.BulletEmitter);
+        //EventHandler.Instance.InvokeOnShotBullet();
     }
-
-    public void FixedUpdateTankController()
+    //player tank movement controller via joystick
+    public void MovementController()
     {
         if (rbTank)
         {
@@ -107,8 +108,8 @@ public class TankController
                 RotationMovement();
             }
         }
-
-        if (true)
+        //due to this true we can move forward and backward when turret in reverse direction.
+        if (TankView.Turret)
         {
             if (rightJoystick.Horizontal != 0)
             {
@@ -117,21 +118,21 @@ public class TankController
         }
 
     }
-
+    //for turretrotaion
     private void TurretRotationMovement()
     {
         Vector3 turrentrotation = Vector3.up * rightJoystick.Horizontal * TankModel.TurretRotationRate * Time.deltaTime;
 
         TankView.Turret.transform.Rotate(turrentrotation, Space.Self);
     }
-
+    //for playertankrotation
     private void RotationMovement()
     {
         Quaternion rotation = rbTank.transform.rotation * Quaternion.Euler(Vector3.up * leftJoystick.Horizontal * TankModel.RotationRate * Time.deltaTime);
 
         rbTank.MoveRotation(rotation);
     }
-
+    //for playertankMovement.
     private void ForwardMovement()
     {
         Vector3 moveforward = rbTank.transform.position + (leftJoystick.Vertical * rbTank.transform.forward * TankModel.Speed * Time.deltaTime);
