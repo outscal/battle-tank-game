209f09046204c037dbeac4796b6ac36579fed06b
Yogesh Sonawane <96642408+Itachi206@users.noreply.github.com>
2022-04-17 18:03:33 +0530

    added Singleton

---
+++ Battle Tank/Battle Tank Game/Assets/Scripts/TankController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController<T> : MonoBehaviour where T : TankController<T>
+{
+    private static T instance;
+    public static T Instance { get { return instance; } }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this);
+            instance = (T)this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+public class PlayerTank : TankController<PlayerTank>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+}
