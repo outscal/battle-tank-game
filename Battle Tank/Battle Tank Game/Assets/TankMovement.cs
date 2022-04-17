921d4758d18aadd5b7d8e5f19632c14ee286fa9e
Yogesh Sonawane <96642408+Itachi206@users.noreply.github.com>
2022-04-18 00:31:51 +0530

    added player movement

---
+++ Battle Tank/Battle Tank Game/Assets/TankMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : TankControll<TankMovement>
+{
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalDirection = Input.GetAxis("Horizontal");
+        float verticalDirection = Input.GetAxis("Vertical");
+
+
+        Vector3 moveDirection = new Vector3(horizontalDirection * Time.deltaTime * speed, 0.0f, verticalDirection * Time.deltaTime * speed);                  //dont want to move tank up and down
+
+        transform.position += moveDirection;
+    }
+}
