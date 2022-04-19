73890825d6bee1cdc4d2fc87e2fc802339bba796
Amit8844 <amitkrsingh1995.sup@gmail.com>
2022-04-19 17:02:42 +0530

    movement
    All direction movement
    Floating Joystick

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody), typeof (BoxCollider))]
 public class TankController : SingletonGeneric<TankController>
 {
+     [SerializeField] private Rigidbody _rigidbody;
+     [SerializeField] private FloatingJoystick _joystick;
+     [SerializeField] private float _moveSpeed ;
 
-    public FixedJoystick joystick;
-    public float MoveSpeed = 2f;
 
-    private Rigidbody _rigidbody;
+
+    
 
     private void FixedUpdate()
     {
-        float XAxisMovement = joystick.Horizontal * MoveSpeed;
-        float ZAxisMovement = joystick.Vertical * MoveSpeed;
+        _rigidbody.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, _joystick.Vertical * _moveSpeed);
 
-        transform.position += new Vector3(XAxisMovement, 0, ZAxisMovement);
+       if  (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+
+            {
+                transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);
+            }
+
     }
 }
