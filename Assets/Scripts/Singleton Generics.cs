d9008ecbee982394c251ab571f6f3dd148980ff3
Amit8844 <amitkrsingh1995.sup@gmail.com>
2022-04-19 14:43:27 +0530

    Joystick
    * Added Player Tank Singleton script
    * Added Enemy Tank Singleton Script
    *Added Joystick For Movement
    *Added Tank Controller script

--- Assets/Scripts/Singleton Generics.cs
+++ Assets/Scripts/Singleton Generics.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
+
 /// This is a Generic Singleton Class which can be inherited to make that child class a Singleton.
-/// </summary>
+
 public class SingletonGeneric<T> : MonoBehaviour where T : SingletonGeneric<T>
 {
     public static T instance;
