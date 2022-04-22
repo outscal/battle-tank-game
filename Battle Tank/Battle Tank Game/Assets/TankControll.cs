3ddad434f051ba09dc6b35b175a805be674bcfbf
Yogesh Sonawane <96642408+Itachi206@users.noreply.github.com>
2022-04-23 01:49:07 +0530

    change some project settings

---
+++ Battle Tank/Battle Tank Game/Assets/TankControll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T instance;
+    public static T Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = (T)this;
+            //DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
