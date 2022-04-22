20cfe5290614001528d081f540efefb73408b949
RK1435 <bhatrajuravikiran@gmail.com>
2022-04-23 04:21:54 +0530

    Tank Controller
    1. Want to add the Tank Model and View.

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// In MVC there won't be any script is Singleton, rather than that MVC-S the S mean service which controlls total MVC will be a Singleton
+// This need to be Tank Controller
+// Tank View will be attached to the Game Object and Tank controller will be called in the Tank View
+// Like Tank View there will be Tank Model which will be Monobehaviour(as it should contain Unity Engine header coz it uses some services of it) but not attached to any Game Object 
 public class PlayerTankController: TankGenericSingleton<PlayerTankController>
 {
     public float mSpeed = 5.0f;
