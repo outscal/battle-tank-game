567d4c62ccd2bda8dabd50a32fba80229f2b339d
Yogesh Sonawane <96642408+Itachi206@users.noreply.github.com>
2022-04-26 19:27:36 +0530

    Rename the Generic Mono Singleton file

---
+++ Battle Tank/Battle Tank Game/Assets/Scripts/MonoGenericSingleton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoGenericSingleton<T> : MonoBehaviour where T : MonoGenericSingleton<T>
+{
+    private static T instance;
+    public static T Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = (T)this;
+            //DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
