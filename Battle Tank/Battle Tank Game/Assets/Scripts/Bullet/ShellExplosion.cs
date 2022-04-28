df76bb9df70f9791a191d98e904937e354e0104b
Yogesh Sonawane <96642408+Itachi206@users.noreply.github.com>
2022-04-28 19:06:46 +0530

    Added shell

---
+++ Battle Tank/Battle Tank Game/Assets/Scripts/Bullet/ShellExplosion.cs
@@ -0,0 +1,63 @@
+using System;
+using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask TankMask;
+    public ParticleSystem explosionParticles;
+    public AudioSource explosionAudio;
+    public float maxDamage = 100f;
+    public float explosionForce = 1000f;
+    public float maxLifeTime = 2f;
+    public float explosionRadius = 5f;
+    
+    void Start()
+    {
+        Destroy(gameObject, maxLifeTime);
+    }
+
+    [Obsolete]
+    private void OnTriggerEnter(Collider other)
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius, TankMask); 
+
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+            if(!targetRigidbody)
+                continue;
+            
+            targetRigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+
+            // reduce tank health using takedamage function of tank
+            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+                if(!targetHealth)
+                    continue;
+
+            float damage = CalculateDamage(targetRigidbody.position);
+
+            targetHealth.TakeDamage(damage);
+        }
+
+        explosionParticles.transform.parent = null;
+
+        explosionParticles.Play();
+        explosionAudio.Play();
+
+        Destroy(explosionParticles.gameObject, explosionParticles.duration);
+        Destroy(gameObject);
+    }
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        Vector3 explosionToTarget = targetPosition - transform.position;
+        float explosionDistance = explosionToTarget.magnitude;
+        float relativeDistance = (explosionRadius - explosionDistance) / explosionRadius;
+
+        float damage = relativeDistance * maxDamage;
+        damage = Mathf.Max(0f, damage);
+        
+        return damage;
+    }
+}
