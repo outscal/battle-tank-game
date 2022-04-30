04c73c6f214751483a63b48c9c619da5bce994ab
Avinashilingam <64300843+Avinashilingam@users.noreply.github.com>
2022-04-30 16:08:59 +0530

    Commit 5
    Done with Singleton concept

--- Assets/Scripts/NewTankController.cs
+++ Assets/Scripts/NewTankController.cs
@@ -6,15 +6,23 @@ using UnityEngine;
 public class NewTankController : MonoBehaviour
 {
     public float speed = 5;
+    public  float turnspeed = 20;
+    private float tnk_rotation;
+    private float tnk_movement;
 
-    
-   
-    
-    
-   
+    void Update()
+    {
+        GetInput();
 
+    }
    
-    void FixedUpdate()
+    void GetInput()
+    {
+        tnk_movement = Input.GetAxisRaw("vertical");
+        tnk_rotation = Input.GetAxisRaw("horizontal");
+    }
+    
+   void FixedUpdate()
     {
         Tankmovement();
     }
@@ -30,7 +38,7 @@ public class NewTankController : MonoBehaviour
     {
         // How: move tank, horizontally(rotate tank) and vertically
         float x = 0;
-        float y = 20 * Time.fixedDeltaTime;
+        float y = turnspeed *tnk_rotation*Time.fixedDeltaTime;
         float z = 0;
 
         Vector3 Angles = new Vector3(x, y, z);
@@ -41,7 +49,7 @@ public class NewTankController : MonoBehaviour
     private void VerticalMovement()
     {
         // Vertical movement: w and s ; up(direction is z) and down(direction is -z)
-         transform.Translate(Vector3.forward * speed *  Time.fixedDeltaTime);
+       Vector3 movement =  transform.forward * tnk_movement * speed *  Time.fixedDeltaTime;
         
     }
 }
