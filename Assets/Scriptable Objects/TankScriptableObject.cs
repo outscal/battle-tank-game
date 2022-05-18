37180f91a6a63cfe1058a10df609c9f2a88b9bbf
Avinashilingam <64300843+Avinashilingam@users.noreply.github.com>
2022-05-18 22:33:53 +0530

    commit 4
    Started with Scriptable objects

---
+++ Assets/Scriptable Objects/TankScriptableObject.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+
+
+    
+    
+    [CreateAssetMenu(fileName = "TankScriptableObject", menuName = "Scriptableobject/NewTank")]
+    public class TankScriptableObject : ScriptableObject
+    {
+        
+    }
+    
+
