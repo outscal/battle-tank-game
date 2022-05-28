fabbdeb9d4077e8e6e799912316c558086e338e0
Avinashilingam <64300843+Avinashilingam@users.noreply.github.com>
2022-05-28 19:47:31 +0530

    Commit 2
    Working on Enemy MVC

---
+++ Assets/Scripts/MVC/Enemy/EnemyController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/MVC/Tank/TankSpawner.cs
+++ Assets/Scripts/MVC/Tank/TankSpawner.cs
@@ -7,20 +7,21 @@ public class TankSpawner : MonoBehaviour
    public TankView tankView;
    //public TankScriptableObject[] tankConfig;
    public TankScriptableObjectList tankList;
+   public int tankTypeIndex;
    
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0;i<3;i++){
-           CreateTank(i); 
-        } 
-      // CreateTank();
+        // for(int i = 0;i<3;i++){
+        //    CreateTank(i); 
+        // } 
+       CreateTank();
     }
 
-    private void CreateTank(int index)
+    private void CreateTank()
     {
-        //TankScriptableObject tankScriptableObject = tankConfig[4];
-        TankScriptableObject tankScriptableObject = tankList.tanks[index];
+        TankScriptableObject tankScriptableObject = tankList.tanks[tankTypeIndex];
+        // TankScriptableObject tankScriptableObject = tankList.tanks[index];
         TankModel tankModel = new TankModel(tankScriptableObject);
         TankController tankController = new TankController(tankModel,tankView);
         
