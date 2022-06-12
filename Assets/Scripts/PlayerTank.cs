858e523d45c82f8dfa5fe91a69f1ceb133a7b8b5
manojkumar4799 <manoj.mutukuloju18@gmail.com>
2022-06-12 13:00:48 +0530

    movement for playerTank

--- Assets/Scripts/PlayerTank.cs
+++ Assets/Scripts/PlayerTank.cs
@@ -1,8 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerTank : GenericSingleton<PlayerTank>
 {
-   //PlayerTank is singleton
+    [SerializeField] float tankSpeed;
+    [SerializeField] float rotationSpeed;    
+
+    private void FixedUpdate()
+    {
+        TankMovement();
+
+    }
+    private void TankMovement()
+    {
+        float forward= Input.GetAxis("Vertical1") *tankSpeed*Time.deltaTime;
+        float tankRoation =Input.GetAxis("Horizontal1"); 
+        transform.Translate( 0,0,forward);
+        transform.Rotate(0, tankRoation*rotationSpeed, 0);
+
+    }
 }
