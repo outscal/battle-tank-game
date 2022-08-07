34653cf202216cddcfb0b80fa6b77da275f19ee1
Avinashilingam <64300843+Avinashilingam@users.noreply.github.com>
2022-08-07 20:12:21 +0530

    commit 12
    Working Player and enemy health logics

--- Assets/Scripts/MVC/Tank/TankView.cs
+++ Assets/Scripts/MVC/Tank/TankView.cs
@@ -25,11 +25,23 @@ public class TankView : MonoBehaviour
      public float Startinghealth = 100f;
     public Color m_FullHealthColor = Color.green;
     public Color m_ZeroHealthColor = Color.red;
+
+     public AudioSource m_ExplosionAudio;               // The audio source to play when the tank explodes.
+    public ParticleSystem m_ExplosionParticles;        // The particle system the will play when the tank is destroyed.
+
+        public Rigidbody m_Shell;                   // Prefab of the shell.
+    public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
+    public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
+    public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
+    public AudioClip m_ChargingClip;            // Audio that plays when each shot is charging up.
+    public AudioClip m_FireClip;                // Audio that plays when each shot is fired.
+
+
     
      
     // Tank Death
-    //  private bool tankDead;
-    // public GameObject m_ExplosionPrefab;
+      private bool tankDead;
+     public GameObject m_ExplosionPrefab;
     
     
    
@@ -45,7 +57,7 @@ public class TankView : MonoBehaviour
      public void OnEnable()
     {
         currentHealth = Startinghealth;
-        // tankDead = false;
+         tankDead = false;
         SetHealthUI();
        
      
@@ -72,6 +84,17 @@ public class TankView : MonoBehaviour
         
     }
 
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        SetHealthUI();
+
+        if (currentHealth <= 0f && !tankDead)
+        {
+            OnDeath();
+        }
+    }
+
 
    // User Input
     private void GetInput()
@@ -88,6 +111,26 @@ public class TankView : MonoBehaviour
         healthFill.fillAmount = Mathf.Lerp(healthFill.fillAmount,currentHealth / Startinghealth, lerpSpeed);
         Color Healthcolor = Color.Lerp(m_ZeroHealthColor,m_FullHealthColor,(currentHealth/Startinghealth));
         healthFill.color = Healthcolor;
+    }
+
+    private void OnDeath()
+    {
+
+        tankDead = true;
+        // m_ExplosionParticles.transform.position = transform.position;
+         m_ExplosionParticles.gameObject.SetActive(true);
+
+        // // Play the particle system of the tank exploding.
+         m_ExplosionParticles.Play();
+
+        // // Play the tank explosion sound effect.
+         m_ExplosionAudio.Play();
+
+        // Turn the tank off.
+        gameObject.SetActive(false);
+
+        Destroy(gameObject);
+
     }
 
 
