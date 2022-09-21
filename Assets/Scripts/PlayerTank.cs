62137e0b497f8ad64d6eecd1cb79a78e0116eaa6
Vimarsh Sharma <42619785+Vimu-Sama@users.noreply.github.com>
2022-09-21 23:33:03 +0530

    Added Tank Movement except the Tank Rotation in moveDirection

--- Assets/Scripts/PlayerTank.cs
+++ Assets/Scripts/PlayerTank.cs
@@ -4,9 +4,42 @@ using UnityEngine;
 
 public class PlayerTank : GenericSingleton<PlayerTank>
 {
+    [SerializeField] float movSpeed = 5f;
+    [SerializeField] float rotationSpeed= 5f ;
+    [SerializeField] Joystick joystick;
+    float horizontalInput, verticalInput;
+    float requiredAngle;
+    Vector3 moveVector = Vector3.zero; //to avoid reinitialization of memory with every update
     private void Awake() //simple implementation
     {
-        Debug.Log("called singleton from child");
-        GenericSingleton<PlayerTank>.Instance.MakeSingleton();
+        base.MakeSingleton();
     }
+
+    private void Update()
+    {
+        horizontalInput = joystick.Horizontal; //Input.GetAxisRaw("HorizontalUI") ;
+        verticalInput = joystick.Vertical;//Input.GetAxisRaw("VerticalUI");
+        if(horizontalInput>0.2f)
+        {
+            moveVector.x = transform.position.x + movSpeed * Time.deltaTime;
+        }
+        else if(horizontalInput<(-0.2f))
+        {
+            moveVector.x = transform.position.x + (-1) * movSpeed * Time.deltaTime;
+        }
+
+        if(verticalInput>0.2f)
+        {
+            moveVector.z = transform.position.z + movSpeed * Time.deltaTime;
+        }
+        else if(verticalInput<(-0.2f))
+        {
+            moveVector.z = transform.position.z + (-1) * movSpeed * Time.deltaTime;
+        }
+        transform.position = moveVector;
+        
+    }
+
+    
+
 }
