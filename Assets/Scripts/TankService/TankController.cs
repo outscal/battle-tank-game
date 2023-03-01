7692dbf5b224063c761d1c1a0691601edb4f5179
praksahb <39479839+praksahb@users.noreply.github.com>
2023-03-02 04:07:46 +0530

    cleanup code

--- Assets/Scripts/TankService/TankController.cs
+++ Assets/Scripts/TankService/TankController.cs
@@ -6,6 +6,7 @@ public class TankController
     public TankView tankView { get; }
 
     private Rigidbody rb;
+    private Transform transform;
 
     public TankController(TankModel _tankModel, TankView tankPrefab)
     {
@@ -15,13 +16,11 @@ public class TankController
 
     public void Move(Vector2 _moveDirection)
     {
-        //Vector3 moveDirection = new Vector3(_moveDirection.x, 0, _moveDirection.y);
-
         Vector3 moveDirection = _moveDirection.vector2ToVector3();
         Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
         targetRotation = Quaternion.RotateTowards
         (
-        tankView.GetRotation(),
+        tankView.transform.localRotation,
         targetRotation,
             tankModel.RotateSpeed * Time.fixedDeltaTime
         );
@@ -31,16 +30,14 @@ public class TankController
             rb = tankView.GetRigidbody();
         }
         
-        // this can be in tankView will eliminate need to store rigidbody here
         rb.MovePosition(rb.position + moveDirection * tankModel.Speed * Time.deltaTime);
         rb.MoveRotation(targetRotation);
     }
-
     public void Jump()
     {
-        if(!rb)
+        if (!rb)
         {
-            rb = tankView.GetRigidbody();
+            rb = tankView.GetComponent<Rigidbody>();
         }
         rb.AddForce(Vector3.up * tankModel.JumpForce, ForceMode.Impulse);
     }
