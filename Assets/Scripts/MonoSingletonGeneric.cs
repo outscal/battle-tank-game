29536e34dd37adb32252f6b6263767a5f34aa03d
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-06-12 10:33:52 +0530

    Added MonoSingleton Generic

---
+++ Assets/Scripts/MonoSingletonGeneric.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingletonGeneric<T> : MonoBehaviour where T : MonoSingletonGeneric<T>
+{
+    private static T instance;
+    public static T Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = (T)this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
