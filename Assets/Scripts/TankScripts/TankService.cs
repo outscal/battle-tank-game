b5a4914673814863754e4d18ff1ba8a37851689d
RohithKannan <51419779+RohithKKannan@users.noreply.github.com>
2023-06-23 05:33:03 +0530

    Added Scriptable Objects for Enemy Tanks
    	- Added three prefabs for enemy tanks
    	- Added a seperate scriptable object list for enemy tank types
    	- Modified Tank service to include both player and enemy tank
    	  scriptable objects

--- Assets/Scripts/TankScripts/TankService.cs
+++ Assets/Scripts/TankScripts/TankService.cs
@@ -5,23 +5,25 @@ public enum TankType
 }
 public class TankService : GenericSingleton<TankService>
 {
-    [SerializeField] TankScriptableObjectList tankList;
+    [SerializeField] TankScriptableObjectList playerTankList;
+    [SerializeField] TankScriptableObjectList enemyTankList;
     [SerializeField] int enemyCount = 2;
     [SerializeField] FixedJoystick joystick;
     [SerializeField] CameraController mainCamera;
     void Start()
     {
-        CreatePlayerTank();
+        CreatePlayerTank(Random.Range(0, playerTankList.tanks.Length));
         for (int i = 0; i < enemyCount; i++)
-            CreateEnemyTank(Random.Range(0, tankList.tanks.Length));
+            CreateEnemyTank(Random.Range(0, enemyTankList.tanks.Length));
     }
-    public void CreatePlayerTank()
+    public void CreatePlayerTank(int index)
     {
-        TankScriptableObject tank = tankList.tanks[Random.Range(0, tankList.tanks.Length)];
+        TankScriptableObject tank = playerTankList.tanks[index];
         TankController tankController = new TankController(tank, TankType.Player, joystick, mainCamera);
     }
     public void CreateEnemyTank(int index)
     {
-        TankController tankController = new TankController(tankList.tanks[index], TankType.Enemy, null, null, 10, 4);
+        TankScriptableObject tank = enemyTankList.tanks[index];
+        TankController tankController = new TankController(tank, TankType.Enemy, null, null, 10, 4);
     }
 }
