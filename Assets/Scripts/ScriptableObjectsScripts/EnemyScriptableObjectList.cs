4014935134b775c677b76fe4b1c1fe4eaa0ac27b
RohithKannan <51419779+RohithKKannan@users.noreply.github.com>
2023-07-01 08:37:13 +0530

    Setup Enemy Spawner
    	- Created enemy scriptable object assets
    	- Created a enemy spawner gameobject with enemy service

---
+++ Assets/Scripts/ScriptableObjectsScripts/EnemyScriptableObjectList.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyScriptableObject", menuName = "ScriptableObjects/NewEnemyTank")]
+public class EnemyScriptableObject : ScriptableObject
+{
+    public int health;
+    public int speed;
+    public BulletType bulletType;
+    public EnemyView enemyView;
+}
+
+[CreateAssetMenu(fileName = "EnemyScriptableObjectList", menuName = "ScriptableObjects/NewEnemyObjectList")]
+public class EnemyScriptableObjectList : ScriptableObject
+{
+    public EnemyScriptableObject[] enemies;
+}
