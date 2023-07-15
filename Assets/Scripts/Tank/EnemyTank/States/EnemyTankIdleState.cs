5c4f4289207dbe94edbb6934efeb13dd0651f180
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-07-15 12:22:02 +0530

    Changed Idel and petrol Behaviour

--- Assets/Scripts/Tank/EnemyTank/States/EnemyTankIdleState.cs
+++ Assets/Scripts/Tank/EnemyTank/States/EnemyTankIdleState.cs
@@ -1,26 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
 public class EnemyTankIdleState : EnemyTankState
 {
-
+    private CancellationTokenSource cancellationTokenSource;
     public override void OnStateEnter()
     {
         base.OnStateEnter();
+        cancellationTokenSource = new CancellationTokenSource();
         ChangeToPetrolState();
     }
 
     private async void ChangeToPetrolState()
     {
-        await Task.Delay(2000);
-        tankView.ChangeState(tankView.petrolState);
+            await Task.Delay(2000, cancellationTokenSource.Token);
+            tankView.ChangeState(tankView.petrolState);
+        
     }
-
     public override void OnStateExit()
     {
         base.OnStateExit();
+        cancellationTokenSource.Cancel();
+
     }
 }
