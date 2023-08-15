75debbff4bb12726f3d7868c78acd5fa48ee5fa9
Hareesh P <46471092+hareeshp007@users.noreply.github.com>
2023-08-15 10:52:54 +0530

    Singleton
    Player and Enemy

--- Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
+++ Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using UnityEngine.PostProcessing;
 
+
 namespace UnityEditor.PostProcessing
 {
-    [CustomPropertyDrawer(typeof(MinAttribute))]
+    [CustomPropertyDrawer(typeof(UnityEngine.PostProcessing.MinAttribute))]
     sealed class MinDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            MinAttribute attribute = (MinAttribute)base.attribute;
+            UnityEngine.MinAttribute attribute = (UnityEngine.MinAttribute)base.attribute;
 
             if (property.propertyType == SerializedPropertyType.Integer)
             {
