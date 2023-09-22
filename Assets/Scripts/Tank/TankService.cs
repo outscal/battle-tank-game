e2ce2f85cc329b2fe135df6f591a24c2e322754e
CreatorsDevs <creatorsdevs@gmail.com>
2023-09-22 15:53:40 +0530

    Established TankSevice using Generic Singleton Class

--- Assets/Scripts/Tank/TankService.cs
+++ Assets/Scripts/Tank/TankService.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TankService : MonoBehaviour
+public class TankService : SingletonGeneric<TankService>
 {
     [SerializeField] private TankScriptableObjectList tankScriptableObjectList;
 
