3cf249c2c6cf89bc71e8d4877018097558810789
Aditya Singh <s.addy1317@gmail.com>
2023-09-27 21:49:20 +0530

    Game Update : - Android Build - Tank Movement

---
+++ Assets/Scripts/Tanks/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleTank
+{
+    public class PlayerController : MonoBehaviour
+    {
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
