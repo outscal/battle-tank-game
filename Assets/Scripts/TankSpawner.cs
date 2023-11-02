fcbb832ffe868c22f20d43457b856d25d905dfd1
destructodroid <114401915+destructodroid@users.noreply.github.com>
2023-11-03 00:09:47 +0530

    Tank Selection and Different Types of Tanks Spawning Added

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -20,13 +20,28 @@ public class TankSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CreateTank();
+
+
     }
 
-    private void CreateTank()
+    public void CreateTank(TankTypes tankType)
     {
-        TankModel tankModel = new TankModel(tankList[1].movementSpeed, tankList[1].rotationSpeed, tankList[1].tankType, tankList[1].color);
-        TankController tankController = new TankController(tankModel, tankView);
+        if (tankType == TankTypes.BlueTank)
+        {
+            TankModel tankModel = new TankModel(tankList[1].movementSpeed, tankList[1].rotationSpeed, tankList[1].tankType, tankList[1].color);
+            TankController tankController = new TankController(tankModel, tankView);
+        }
+        if (tankType == TankTypes.RedTank)
+        {
+            TankModel tankModel = new TankModel(tankList[0].movementSpeed, tankList[0].rotationSpeed, tankList[0].tankType, tankList[0].color);
+            TankController tankController = new TankController(tankModel, tankView);
+        }
+        if (tankType == TankTypes.GreenTank)
+        {
+            TankModel tankModel = new TankModel(tankList[2].movementSpeed, tankList[2].rotationSpeed, tankList[2].tankType, tankList[2].color);
+            TankController tankController = new TankController(tankModel, tankView);
+        }
+        
     }
 
 }
